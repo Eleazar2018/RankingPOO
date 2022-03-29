5ab78bccf113ef1b0c5603aff5c69133951b3e28
Eleazar Ribeiro <eliribeiro2010@gmail.com>
2022-03-29 14:14:35 -0300

    Criado projeto Estrutura de dados

---
+++ src/main/java/com/example/fx/HelloController.java
@@ -0,0 +1,30 @@
+package com.example.fx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    private TextField txtUser;
+
+    @FXML
+    private PasswordField txtPass;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private Button btnRegistrar;
+
+    public void OnbtnRegistrar() {
+
+    }
+
+
+}
